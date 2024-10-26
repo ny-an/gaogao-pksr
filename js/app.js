13a40a2ca918d4cd7ac2d8a98de2e79c0f3ce4fe
nyama <nyama@neruko.net>
2024-10-26 22:21:45 +0900

    jsの共通関数を移行

--- js/app.js
+++ js/app.js
@@ -20,50 +20,6 @@ categoryRadios.forEach(radio => {
 // 必要食材リストを更新
 foodSelect.addEventListener('change', updateIngredients);
 
-function updateIngredients() {
-  const selectedCategory = document.querySelector('input[name="category"]:checked')?.value;
-  const selectedDish = foodSelect.value;
-
-  ingredientTableBody.innerHTML = ""; // テーブルの内容を初期化
-  let sum1 = 0, sum3 = 0, sum15 = 0, sum21 = 0;
-
-  if (selectedCategory && selectedDish && dishes[selectedCategory] && dishes[selectedCategory][selectedDish]) {
-    const ingredients = dishes[selectedCategory][selectedDish];
-
-    for (const [ingredient, amount] of Object.entries(ingredients)) {
-      const row = document.createElement('tr');
-      const amount1 = amount;
-      const amount3 = amount * 3;
-      const amount15 = amount * 15;
-      const amount21 = amount * 21;
-
-      sum1 += amount1;
-      sum3 += amount3;
-      sum15 += amount15;
-      sum21 += amount21;
-
-      // 画像のHTMLコードを生成
-      const imgPath = getIngredientImagePath(ingredient);
-      const imgTag = `<img src="${imgPath}" alt="${ingredient}" style="width: 30px; height: 30px;">`;
-
-      row.innerHTML = `
-                <td>${imgTag}</td>
-                <td>${amount1}</td>
-                <td>${amount3}</td>
-                <td>${amount15}</td>
-                <td>${amount21}</td>
-            `;
-      ingredientTableBody.appendChild(row);
-    }
-  }
-
-  // 合計の表示を更新
-  total1.textContent = sum1;
-  total3.textContent = sum3;
-  total15.textContent = sum15;
-  total21.textContent = sum21;
-}
-
 // 初期表示の設定
 updateIngredients();
 
--- js/common.js
+++ js/common.js
@@ -24,7 +24,7 @@ function getIngredientImagePath(ingredient) {
 
 
 
-// 料理リストを更新する関数
+// カテゴリ選択時に料理リストを更新する関数
 function updateFoodOptions(selectedCategory) {
   foodSelect.innerHTML = '<option value="">-- 料理を選択 --</option>';
 
@@ -64,4 +64,50 @@ function sortDishesByTotalIngredients(dishes) {
   }
 
   return sortedDishes;
+}
+
+
+// 料理選択時のテーブル更新
+function updateIngredients() {
+  const selectedCategory = document.querySelector('input[name="category"]:checked')?.value;
+  const selectedDish = foodSelect.value;
+
+  ingredientTableBody.innerHTML = ""; // テーブルの内容を初期化
+  let sum1 = 0, sum3 = 0, sum15 = 0, sum21 = 0;
+
+  if (selectedCategory && selectedDish && dishes[selectedCategory] && dishes[selectedCategory][selectedDish]) {
+    const ingredients = dishes[selectedCategory][selectedDish];
+
+    for (const [ingredient, amount] of Object.entries(ingredients)) {
+      const row = document.createElement('tr');
+      const amount1 = amount;
+      const amount3 = amount * 3;
+      const amount15 = amount * 15;
+      const amount21 = amount * 21;
+
+      sum1 += amount1;
+      sum3 += amount3;
+      sum15 += amount15;
+      sum21 += amount21;
+
+      // 画像のHTMLコードを生成
+      const imgPath = getIngredientImagePath(ingredient);
+      const imgTag = `<img src="${imgPath}" alt="${ingredient}" style="width: 30px; height: 30px;">`;
+
+      row.innerHTML = `
+                <td>${imgTag}</td>
+                <td>${amount1}</td>
+                <td>${amount3}</td>
+                <td>${amount15}</td>
+                <td>${amount21}</td>
+            `;
+      ingredientTableBody.appendChild(row);
+    }
+  }
+
+  // 合計の表示を更新
+  total1.textContent = sum1;
+  total3.textContent = sum3;
+  total15.textContent = sum15;
+  total21.textContent = sum21;
 }
