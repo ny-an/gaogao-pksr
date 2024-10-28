c1076bb70d8d664858fa6f01abaf9b4052adcddf
nyama <nyama@neruko.net>
2024-10-28 19:04:57 +0900

    クレジットmodalを追加

--- js/app.js
+++ js/app.js
@@ -23,3 +23,27 @@ document.addEventListener('DOMContentLoaded', () => {
   selectFirstFoodOption();
 
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const settingsIcon = document.querySelector('.settings-icon');
+  const creditModal = document.getElementById('creditModal');
+  const closeModal = document.querySelector('.close');
+
+  // 設定アイコンをクリックしたときにモーダルを表示
+  settingsIcon.addEventListener('click', () => {
+    creditModal.style.display = 'block';
+  });
+
+  // モーダルの「×」ボタンをクリックしてモーダルを閉じる
+  closeModal.addEventListener('click', () => {
+    creditModal.style.display = 'none';
+  });
+
+  // モーダルの外側をクリックしてモーダルを閉じる
+  window.addEventListener('click', (event) => {
+    if (event.target === creditModal) {
+      creditModal.style.display = 'none';
+    }
+  });
+});
