3128ebb6df377a98aa0d7051fdc46c4573031dd2
nyama <nyama@neruko.net>
2025-02-16 00:28:16 +0900

    設定モーダル：jsの管理を最適化するため、設定アイコン関連の処理を`setting_icon.js`に分離し、各HTMLにスクリプトを適切に読み込むよう修正

--- js/app.js
+++ js/app.js
@@ -37,37 +37,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-
-// 設定アイコン
-document.addEventListener('DOMContentLoaded', () => {
-  const settingsIcon = document.querySelector('.settings-icon');
-  const creditModal = document.getElementById('creditModal');
-  const closeModal = document.querySelector('.close');
-
-  // 設定アイコンをクリックしたときにモーダルを表示
-  settingsIcon.addEventListener('click', () => {
-    creditModal.style.display = 'block';
-
-    // 花火だドン！
-    startSideFire();
-  });
-
-  // モーダルの「×」ボタンをクリックしてモーダルを閉じる
-  closeModal.addEventListener('click', () => {
-    creditModal.style.display = 'none';
-  });
-
-  // モーダルの外側をクリックしてモーダルを閉じる
-  window.addEventListener('click', (event) => {
-    if (event.target === creditModal) {
-      creditModal.style.display = 'none';
-    }
-  });
-
-  // SPタッチイベント対応：モーダルの外側をクリックしてモーダルを閉じる
-  window.addEventListener('touchstart', (event) => {
-    if (event.target === creditModal) {
-      creditModal.style.display = 'none';
-    }
-  });
-});
